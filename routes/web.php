8895ac96cf3408c561133779b8f3fd9ac55d42ca
ryo-1121 <ryo.yamaya@gmail.com>
2021-01-11 05:25:54 +0000

    samazon完成版

--- routes/web.php
+++ routes/web.php
@@ -19,9 +19,7 @@ URL::forceScheme('https');
 Route::get('/', 'WebController@index');
 
 Route::get('users/carts', 'CartController@index')->name('carts.index');
-
 Route::post('users/carts', 'CartController@store')->name('carts.store');
-
 Route::delete('users/carts', 'CartController@destroy')->name('carts.destroy');
 
 Route::get('users/mypage', 'UserController@mypage')->name('mypage');
@@ -31,15 +29,35 @@ Route::put('users/mypage', 'UserController@update')->name('mypage.update');
 Route::get('users/mypage/favorite', 'UserController@favorite')->name('mypage.favorite');
 Route::get('users/mypage/password/edit', 'UserController@edit_password')->name('mypage.edit_password');
 Route::put('users/mypage/password', 'UserController@update_password')->name('mypage.update_password');
+Route::delete('users/mypage/delete', 'UserController@destroy')->name('mypage.destroy');
+Route::get('users/mypage/cart_history', 'UserController@cart_history_index')->name('mypage.cart_history');
+Route::get('users/mypage/cart_history/{num}', 'UserController@cart_history_show')->name('mypage.cart_history_show');
+Route::get('users/mypage/register_card', 'UserController@register_card')->name('mypage.register_card');
+Route::post('users/mypage/token', 'UserController@token')->name('mypage.token');
 
 
 Route::post('products/{product}/reviews', 'ReviewController@store');
 
 Route::get('products/{product}/favorite', 'ProductController@favorite')->name('products.favorite');
-Route::resource('products', 'ProductController');
+
+Route::get('products', 'ProductController@index')->name('products.index');
+Route::get('products/{product}', 'ProductController@show')->name('products.show');
+
 Auth::routes(['verify' => true]);
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/dashboard', 'DashboardController@index')->middleware('auth:admins');
+
+Route::group(['prefix' => 'dashboard', 'as' => 'dashboard.'], function () {
+    Route::get('login', 'Dashboard\Auth\LoginController@showLoginForm')->name('login');
+    Route::post('login', 'Dashboard\Auth\LoginController@login')->name('login');
+    Route::resource('major_categories', 'Dashboard\MajorCategoryController')->middleware('auth:admins');
+    Route::resource('categories', 'Dashboard\CategoryController')->middleware('auth:admins');
+    Route::resource('products', 'Dashboard\ProductController')->middleware('auth:admins');
+    Route::resource('users', 'Dashboard\UserController')->middleware('auth:admins');
+    Route::get('orders', 'Dashboard\OrderController@index')->middleware('auth:admins');
+});
 
 if (env('APP_ENV') === 'production') {
      URL::forceScheme('https');
